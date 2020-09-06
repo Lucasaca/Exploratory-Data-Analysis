a8b0db80c3133163183f74c86e8aecb90982c859
GeoLuCaS <53106492+Lucasaca@users.noreply.github.com>
2020-09-06 11:39:17 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -4,9 +4,9 @@
 data <- read.table(df, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")                           ## Data
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]                                           ## Data between the two days
 globalActivePower <- as.numeric(subSetData$Global_active_power)                                         ## Data that you're going to use for the histogram
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)                                                   ## SubMetering1
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)                                                   ## SubMetering2
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)                                                   ## SubMetering3
 
 png("plot3.png", width=480, height=480)                                                                 ## PNG Image 480 pixels x 480 pixels
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")                              ## Plot + Line 1
